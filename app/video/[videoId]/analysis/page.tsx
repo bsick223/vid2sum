454e23b3fd4a00ca6950992bc94cb4644252dcec
Brendan Sick <brendansick@gmail.com>
2025-02-28 01:59:20 -0800

    Created youtube hooks and display with API

--- app/video/[videoId]/analysis/page.tsx
+++ app/video/[videoId]/analysis/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import Usage from "@/components/Usage";
+import YoutubeVideoDetails from "@/components/YoutubeVideoDetails";
+import { FeatureFlag } from "@/features/flags";
+import { useParams } from "next/navigation";
+
 function AnalysisPage() {
+  const params = useParams<{ videoId: string }>();
+  const { videoId } = params;
+
   return (
     <div className="xl:container mx-auto px-4 md:px-0">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {/* Left Side */}
-        <div className="order-2 lg:order-1">
+        <div
+          className="order-2 lg:order-1 flex flex-col 
+        gap-4 bg-white lg:border-r border-gray-200 p-6"
+        >
           {/* Analysis Section */}
+
+          <div className="flex flex-col gap-4 p-4 border border-gray-200 rounded-xl">
+            <Usage
+              featureFlag={FeatureFlag.ANALYSE_VIDEO}
+              title="Analyse Video"
+            />
+            {/* Video Transcription Status */}
+          </div>
           {/* Youtube Video Details */}
+          <YoutubeVideoDetails videoId={videoId} />
+
           {/* Thumbnail Generation */}
           {/* Title Generation */}
           {/* Transcription */}
@@ -13,8 +36,8 @@ function AnalysisPage() {
         </div>
 
         {/* Right Side */}
-        <div className="order-1 lg:order-2">
-            {/* Ai Agent Chat Section */}
+        <div className="order-1 lg:order-2 lg:sticky lg:top-20 h-[h500px] md:h-[calc(100vh-6rem)]">
+          {/* Ai Agent Chat Section */}
           <p>Chat</p>
         </div>
       </div>
---
+++ types/types.ts
@@ -0,0 +1,20 @@
+
+
+export interface ChannelDetails {
+    title: string;
+    thumbnail: string;
+    subscribers: string;
+  }
+  
+
+// Complete video details
+export interface VideoDetails {
+    title: string;
+    views: string;
+    likes: string;
+    comments: string;
+    thumbnail: string;
+    channel: ChannelDetails;
+    publishedAt: string;
+  }
+  
