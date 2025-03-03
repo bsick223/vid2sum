0c9ad51b348e44700abed21caf357d966630cd4b
Brendan Sick <brendansick@gmail.com>
2025-03-02 23:47:20 -0800

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ClientWrapper from "@/components/ClientWrapper";
 import Header from "@/components/Header";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <ClientWrapper>
           <Header />
           <main>{children}</main>
