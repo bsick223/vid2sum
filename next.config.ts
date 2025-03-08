438065d4241457e1d4a6468c4d34082c7dcda829
Brendan Sick <brendansick@gmail.com>
2025-03-08 15:09:19 -0800

    pre-update

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
       },
       {
-        hostname: "elegant-starfish-986.convex.cloud",
+        hostname: "small-buzzard-941.convex.cloud",
         protocol: "https",
       },
     ],
