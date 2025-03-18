fe302f9d9b7b632ed743f4c0658c7849a29325e5
Brendan Sick <brendansick@gmail.com>
2025-03-17 23:17:33 -0700

    refactored the mailchimp.d.ts type file

--- types/mailchimp.d.ts
+++ types/mailchimp.d.ts
@@ -1,16 +1,19 @@
-declare module '@mailchimp/mailchimp_marketing' {
+declare module "@mailchimp/mailchimp_marketing" {
   interface Campaign {
     id: string;
   }
 
-  export default {
-    setConfig: (config: { apiKey: string; server: string }) => void;
+  interface Mailchimp {
+    setConfig(config: { apiKey: string; server: string }): void;
     lists: {
-      addListMember: (listId: string, data: any) => Promise<any>;
+      addListMember(listId: string, data: any): Promise<any>;
     };
     campaigns: {
-      create: (data: any) => Promise<Campaign>;
-      send: (campaignId: string) => Promise<any>;
+      create(data: any): Promise<Campaign>;
+      send(campaignId: string): Promise<any>;
     };
-  };
-} 
+  }
+
+  const mailchimp: Mailchimp;
+  export default mailchimp;
+}
